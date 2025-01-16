9ba0a926f91f869fee0402fb48347fb0162377ca
Thomas <t.o.c.cheng@student.tudelft.nl>
2025-01-16 10:30:42 +0100

    added pepijns and my keymaps

--- keyboards/ferris/keymaps/thomasc791/config.h
+++ keyboards/ferris/keymaps/thomasc791/config.h
@@ -36,7 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 0
-#define SPLIT_USB_DETECT
 
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
